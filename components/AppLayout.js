836598b0d541444d3a8f9c972212e3c0546970fb
jeongin <kwjddls1919@naver.com>
2022-11-16 23:20:49 +0900

    Design: AppLayout 컴포넌트 디자인 변경

--- components/AppLayout.js
+++ components/AppLayout.js
@@ -1,10 +1,15 @@
 import { Button, Layout, Typography } from "antd";
+import Link from "next/link";
 import React from "react";
+import { useSelector } from "react-redux";
+import UserCard from "./UserProfileDropDown";
 
 const { Header, Footer, Sider, Content } = Layout;
 const { Title } = Typography;
 
 const AppLayout = ({ children }) => {
+  const { logInDone } = useSelector((state) => state.user);
+
   return (
     <>
       <Layout>
@@ -12,23 +17,44 @@ const AppLayout = ({ children }) => {
           style={{
             display: "flex",
             justifyContent: "space-between",
+            alignItems: "center",
           }}
         >
-          <Title level={2} style={{ padding: "10px", color: "white" }}>
-            Board
-          </Title>
+          <Link href="/">
+            <a>
+              <Title level={2} style={{ paddingTop: "10px", color: "white" }}>
+                Board
+              </Title>
+            </a>
+          </Link>
           <div>
-            <Button type="primary" style={{ marginRight: "10px" }}>
-              로그인
-            </Button>
-            <Button>회원가입</Button>
+            {logInDone ? (
+              <UserCard />
+            ) : (
+              <>
+                <Link href="/login">
+                  <a>
+                    <Button type="primary" style={{ marginRight: "10px" }}>
+                      로그인
+                    </Button>
+                  </a>
+                </Link>
+                <Link href="/signup">
+                  <a>
+                    <Button>회원가입</Button>
+                  </a>
+                </Link>
+              </>
+            )}
           </div>
         </Header>
-        <Layout>
+        <Layout style={{ display: "flex" }}>
           <Sider style={{ background: "white" }}>Sider</Sider>
-          <Content>{children}</Content>
+          <Content style={{ justifyContent: "center", alignItems: "center" }}>
+            {children}
+          </Content>
         </Layout>
-        <Footer>Footer</Footer>
+        <Footer>Copyright 2022. JeongIn All rights reserved.</Footer>
       </Layout>
     </>
   );
