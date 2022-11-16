651b3261d40b0de334bcdd5c37c6b9bfc140ea7e
jeongin <kwjddls1919@naver.com>
2022-11-16 23:32:38 +0900

    Refactor: import 변경

--- pages/login.js
+++ pages/login.js
@@ -1,10 +1,10 @@
 import React from "react";
-import LoginForm from "../components/LoginForm";
+import LogInForm from "../components/LogInForm";
 
 const login = () => {
   return (
     <>
-      <LoginForm />
+      <LogInForm />
     </>
   );
 };
