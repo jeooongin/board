344e3763dd4cb3c53b44386267f8c30ea48d5dd6
jeongin <kwjddls1919@naver.com>
2022-11-16 23:31:59 +0900

    Feat: UserProfileDropDown 컴포넌트 생성

---
+++ components/UserProfileDropDown.js
@@ -0,0 +1,45 @@
+import { CaretDownOutlined, UserOutlined } from "@ant-design/icons";
+import { Avatar, Dropdown, Space } from "antd";
+import Link from "next/link";
+import React, { useCallback } from "react";
+import { useDispatch } from "react-redux";
+
+import { logOut } from "../reducers/user";
+
+const UserProfileDropDown = () => {
+  const dispatch = useDispatch();
+
+  const onSubmitLogOut = useCallback(() => {
+    dispatch(logOut());
+  });
+
+  const items = [
+    {
+      label: (
+        <Link href="/profile">
+          <a>내 정보</a>
+        </Link>
+      ),
+      key: "/profile",
+    },
+    {
+      label: (
+        <Link href="/">
+          <a onClick={onSubmitLogOut}>로그아웃</a>
+        </Link>
+      ),
+      key: "/logout",
+    },
+  ];
+
+  return (
+    <Dropdown menu={{ items }} trigger={["click"]}>
+      <Space>
+        <Avatar icon={<UserOutlined />} />
+        <CaretDownOutlined style={{ color: "white" }} />
+      </Space>
+    </Dropdown>
+  );
+};
+
+export default UserProfileDropDown;
