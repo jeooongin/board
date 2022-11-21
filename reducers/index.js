83df15d832faa7f799b2cca5c233b453621d0147
jeongin <kwjddls1919@naver.com>
2022-11-21 20:35:00 +0900

    Feat: rootReducer에 post 추가

--- reducers/index.js
+++ reducers/index.js
@@ -1,6 +1,7 @@
 import { combineReducers } from "@reduxjs/toolkit";
 import { HYDRATE } from "next-redux-wrapper";
 
+import postSlice from "./post";
 import userSlice from "./user";
 
 const rootReducer = (state, action) => {
@@ -10,6 +11,7 @@ const rootReducer = (state, action) => {
     default: {
       const combineReducer = combineReducers({
         user: userSlice.reducer,
+        post: postSlice.reducer,
       });
       return combineReducer(state, action);
     }
