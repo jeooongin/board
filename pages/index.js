098633259654309e0d47742ac5e2432d330fa2cf
jeongin <kwjddls1919@naver.com>
2022-11-21 20:34:24 +0900

    Design: 공통 레이아웃 추가

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,27 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
+
+import AppLayout from "../components/AppLayout";
+import PostCard from "../components/PostCard";
+
+import { loadPost } from "../reducers/post";
 
 const index = () => {
-  return <div>index</div>;
+  const dispatch = useDispatch();
+  const { mainPost } = useSelector((state) => state.post);
+
+  useEffect(() => {
+    dispatch(loadPost());
+  }, []);
+
+  return (
+    <AppLayout>
+      {mainPost.map((post) => (
+        <PostCard key={post.id} post={post} />
+      ))}
+    </AppLayout>
+  );
 };
 
 export default index;
--- pages/profile.js
+++ pages/profile.js
@@ -1,8 +1,15 @@
 import React from "react";
+import { useSelector } from "react-redux";
+
 import UserProfileForm from "../components/UserProfileForm";
+import LogInForm from "../components/LogInForm";
+import AppLayout from "../components/AppLayout";
 
 const profile = () => {
-  return <UserProfileForm />;
+  const { logInDone } = useSelector((state) => state.user);
+  return (
+    <AppLayout>{logInDone ? <UserProfileForm /> : <LogInForm />}</AppLayout>
+  );
 };
 
 export default profile;
